7cfcdf3fdefba9fd31c464c089f081e161bf9542
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-06 22:29:58 +0300

    issue-1: wip 2

--- khal_vm_cpp/src/frontend.C
+++ khal_vm_cpp/src/frontend.C
@@ -1,2 +1,11 @@
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <iostream>
 using namespace std;
 
+vector<uint32_t> compile(string expression) {
+  vector<uint32_t> result = {1, 2,2};
+  cout << "Frontend: compiling expression {" << expression << " }" << endl;
+  return result;
+};
--- khal_vm_cpp/src/main.C
+++ khal_vm_cpp/src/main.C
@@ -1,11 +1,16 @@
+#include <cstdint>
 using namespace  std;
 #include <iostream>
-
+#include "frontend.hpp"
+#include <vector>
 int main(int argc, char** argv){
-
+  
   if (argc < 2) {
     cout << "KHAL-VM: You must provide an expression as an agrument to make me work for you"<<endl;
   }else {
-    cout << "KHAL-VM: You provided an expression "<< argv[1]<< "  working on it ..."<<endl;
+    cout << "KHAL-VM: You provided an expression " << argv[1]
+         << "  working on it ..." << endl;
+    
+    auto r = compile(argv[1]);
   }
 }

4c7c1a8103b9ded124750c247158b73bfe06d40d
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-06 22:09:21 +0300

    issue-1: wip 1

--- khal_vm_cpp/src/main.C
+++ khal_vm_cpp/src/main.C
@@ -2,5 +2,10 @@ using namespace  std;
 #include <iostream>
 
 int main(int argc, char** argv){
-  cout <<"Hello,VM"<<endl;
+
+  if (argc < 2) {
+    cout << "KHAL-VM: You must provide an expression as an agrument to make me work for you"<<endl;
+  }else {
+    cout << "KHAL-VM: You provided an expression "<< argv[1]<< "  working on it ..."<<endl;
+  }
 }

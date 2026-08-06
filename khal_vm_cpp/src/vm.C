2ee0aece55d1ad7c0a40dd41fc3fc08008b61acd
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-06 23:16:35 +0300

    issue-1: wip 4

--- khal_vm_cpp/src/vm.C
+++ khal_vm_cpp/src/vm.C
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+typedef struct {
+  char tag;
+  char bytes[8];
+}Register;
 void execute(vector<uint32_t> bytecode) {
-  
-  cout <<"VM: executing bytecode [" << bytecode.size()<< "] operants " <<endl;
+  Register registers[256];
+  cout << "VM: executing bytecode [" << bytecode.size() << "] operants "
+       << endl;
+    auto start = std::begin(bytecode) + 1;
+    auto finish = std::end(bytecode);
+    int i = 1;
+    for (auto it = start; it != finish; ++it) {
+      std::cout << *it << " "; // Output: 30 40 50
+      registers[i].tag = 1;
+      registers[i].bytes[0]= *it;
+      
+    }
 }

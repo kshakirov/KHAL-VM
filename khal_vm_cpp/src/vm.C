ff7fcb454feff822793bd31c995d935486212244
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-09-10 22:40:45 +0300

    Complete first integer register operation

--- khal_vm_cpp/src/vm.C
+++ khal_vm_cpp/src/vm.C
@@ -33,22 +33,29 @@ void execute(vector<Instruction> bytecode) {
     case OpCode::LOAD_INT: {
       auto reg_num = it->operands.at(0);
       auto value = it->operands.at(1);
+      assert(reg_num >= 0 && reg_num < 256);
       registers[reg_num].tag = Tag::INT;
       registers[reg_num].payload.integer= value;
       break;
     }
     case OpCode::ADD_INT:{
-
+      
       auto ra = it->operands.at(0);
+      assert(ra >= 0 && ra < 256);
       assert(registers[ra].tag == Tag::INT); // for the time being
       auto rb = it->operands.at(1);
+      assert(rb >= 0 && rb < 256);
       assert(registers[rb].tag == Tag::INT); // for the time being
       auto added = registers[ra].payload.integer +  registers[rb].payload.integer ;
       cout << "ADD:  " <<  registers[ra].payload.integer << " " << registers[rb].payload.integer << " added is  " << added <<endl;
+      registers[ra].payload.integer= added;
+      cout << "Saved to Register "<<endl;
       break;  
     }
     case OpCode::PRINT_INT:{
       auto ra = it->operands.at(0);
+      assert(ra >= 0 && ra < 256);
+      assert(registers[ra].tag == Tag::INT);
       cout << "PRINT_INT:  " <<  registers[ra].payload.integer  << endl;
       break;
       
